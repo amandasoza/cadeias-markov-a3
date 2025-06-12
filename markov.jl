918202c845e794b1ccac226e8aa82e0f167fc3df
Amanda <amandasouza.codes@gmail.com>
2025-06-12 16:21:31 -0300

    Adicionar estados de humor e matriz de transição para cadeia de Markov

---
+++ markov.jl
@@ -0,0 +1,12 @@
+# Estados de humor
+states = ["Feliz", "Neutro", "Triste", "Animado", "Cansado"]
+
+# Matriz de transição 
+P = [
+    0.45  0.25  0.05  0.20  0.05;  # Feliz
+    0.25  0.40  0.15  0.10  0.10;  # Neutro
+    0.05  0.20  0.50  0.05  0.20;  # Triste
+    0.30  0.20  0.05  0.35  0.10;  # Animado
+    0.10  0.25  0.25  0.10  0.30;  # Cansado
+]
+
